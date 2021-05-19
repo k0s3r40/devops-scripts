1f325c6dec8c9bbc86164cc9b373ece666b26f75
Kostadin Slavov <kostadin.slavov@tixi.bg>
2021-05-19 10:40:43 +0300

    Add docker stuff

--- setup/initial_setup.sh
+++ setup/initial_setup.sh
@@ -22,4 +22,37 @@ echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo User $username created
 
-su $username
+sudo su $username
+
+sudo apt install postgresql postgresql-contrib -y
+
+sudo -i -u postgres
+
+sudo apt-get install nginx-full -y
+
+sudo apt-get update
+
+sudo apt install docker.io -y
+
+sudo systemctl enable --now docker
+
+sudo usermod -aG docker $username
+
+echo Docker is installed
+
+docker --version
+
+sudo docker run hello-world
+
+sudo apt install net-tools -y
+
+sudo docker run -d -p 8080:80 docker/getting-started
+
+echo started a docker at port 8080
+
+echo visit website:
+
+echo http://$(hostname -I |cut -d ' ' -f 1):8080
+
+
+
